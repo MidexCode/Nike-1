c482c562d81213cf6c1a23ec7d3b5775722d750f
Oyeleke Toluwanimi Praise <callmimidex@gmail.com>
2024-11-19 11:22:29 -0800

    Working on the FOoter

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import footerlogo from "../../assets/images/footer-logo.svg";
 
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <>
+      <footer className="max-container">
+        <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+          <div className="flex flex-col items-start">
+            <a href="/">
+              <img src={footerlogo} alt="" width={150} height={46} />
+            </a>
+
+            <p className="mt-6 text-base text-white-400 font-montserrat leading-7 sm:max-w-sm">
+              Get shoes ready for the new term at your nearest Nike store. Find
+              Your perfect Size In Store. Get Rewards
+            </p>
+
+            <div className="flex gap-5 items-center mt-8">{socialM}</div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
